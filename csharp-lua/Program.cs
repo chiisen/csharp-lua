8773f5037545e98538f3a3f67fb7ed5bfc3e0cef
sam <samliao1@auforce.com>
2024-06-12 11:22:36 +0800

    ✨ feat: 初始化 C# 與 Lua 混和編程

---
+++ csharp-lua/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSHARP_LUA;
+
+#region 測試NLua
+var luaExample = new LuaExample();
+luaExample.RunLuaScript();
+luaExample.RunLuaRegisterFunction();
+int[] result = luaExample.RunLuaResult();
+#endregion 測試NLua
